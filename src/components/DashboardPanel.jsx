dce14d8dd9c3c47553cd20b924aa7e46319f2f91
Alex Reiffer <awr7648@gmail.com>
2026-02-23 12:33:01 -0500

    Rearranged layout of dashboard for future stats

--- src/components/DashboardPanel.jsx
+++ src/components/DashboardPanel.jsx
@@ -12,40 +12,52 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
   return (
     <Panel title="Dashboard">
       <div style={{ padding: "5%", boxSizing: "border-box" }}>
-        <p style={{ opacity: 0.8, marginTop: 0 }}>
-          This will show analytics (crime counts, trends, etc.) derived from selection + time range.
-        </p>
-
         {!hasActive && !hasInactive ? (
-          <p style={{ opacity: 0.8 }}>Select a boundary to begin.</p>
+          <div style={{display: "flex", flexDirection: "column", alignItems: "center", width: "100%", gap: 8}}>
+            <p style={{ opacity: 0.8, marginTop: 0 }}>
+            This will show analytics (crime counts, trends, etc.) derived from selection + time range.
+            </p>
+            <p style={{ opacity: 0.8 }}>Select a boundary to begin.</p>
+          </div>
         ) : (
-          <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-            {hasActive && selection.mode === "source"? (
-              <p style={{ opacity: 0.95, margin: 0 }}>
-                <strong>{cFL(selection.mode)}</strong> ready to compute stats for{" "}
-                <strong>{selection.name}</strong>.
-              </p>
-            ) : hasActive && selection.mode === "relation" ? (
-              <p style={{ opacity: 0.9, margin: 0 }}>
-                <strong>{cFL(selection.mode)}</strong> ready to compute stats for{" "}
-                <strong>{selection.name}</strong>.
-              </p>
-            ) : (
-              <p style={{ opacity: 0.8, margin: 0 }}>
-                {mode === "source" ? (<strong>Source</strong>):(<strong>Relation</strong>)} selection not chosen yet.
-              </p>
-            )}
-
-            {hasInactive ? (
-              <p style={{ opacity: 0.9, margin: 0 }}>
-                <strong>{cFL(inactiveSelection.mode)}</strong> ready to compute stats for{" "}
-                <strong>{inactiveSelection.name}</strong>.
-              </p>
-            ) : (
-              <p style={{ opacity: 0.8, margin: 0 }}>
-                {inactiveMode === "target" ? (<strong>Target</strong>): inactiveMode === "actual" ? (<strong>Actual</strong>): (<strong>Error Map </strong>)} selection not chosen yet.
-              </p>
-            )}
+         <div style={{ display: "flex", flex: "1 1 auto", flexDirection: "row", width: "100%", gap: 8}}>
+            <div style={{ display: "flex", flex: "1 1 auto", flexDirection: "column", alignItems: "center", width: "100%", gap: 8 }}>
+              {hasActive && selection.mode === "source"? (
+                <div>
+                  {/* Source Map Stats */}
+                  <p style={{ opacity: 0.95, margin: 0 }}>
+                    <strong>{cFL(selection.mode)}</strong> ready to compute stats for{" "}
+                    <strong>{selection.name}</strong>.
+                  </p>
+                </div>
+              ) : hasActive && selection.mode === "relation" ? (
+                <div>
+                  {/* Relation Map Stats */}
+                  <p style={{ opacity: 0.9, margin: 0 }}>
+                    <strong>{cFL(selection.mode)}</strong> ready to compute stats for{" "}
+                    <strong>{selection.name}</strong>.
+                  </p>
+                </div>
+              ) : (
+                <p style={{ opacity: 0.8, margin: 0 }}>
+                  {mode === "source" ? (<strong>Source</strong>):(<strong>Relation</strong>)} selection not chosen yet.
+                </p>
+              )}
+            </div>
+            <div style={{ width: 0, backgroundColor: "rgba(255, 255, 255, 0.7)", border: "1px solid rgba(255, 255, 255, 0.7)", alignSelf: "stretch", gap: 8 }} />
+            <div style={{ display: "flex", flex: "1 1 auto", flexDirection: "column", alignItems: "center", width: "100%", gap: 8 }}>
+              {hasInactive ? (
+                <p  style={{ opacity: 0.9, margin: 0 }}>
+                  <strong>{cFL(inactiveSelection.mode)}</strong> ready to compute stats for{" "}
+                  <strong>{inactiveSelection.name}</strong>.
+                </p>
+              ) : (
+                <p style={{ opacity: 0.8, margin: 0 }}>
+                  {inactiveMode === "target" ? (<strong>Target</strong>): inactiveMode === "actual" ? (<strong>Actual</strong>): (<strong>Error Map </strong>)} selection not chosen yet.
+                </p>
+              )}
+                
+            </div>
           </div>
         )}
       </div>
