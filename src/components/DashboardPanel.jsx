18f794d914595c32b1fb0ee737e8dcb28f4da183
Mario Ordonez <mario.ordonez.cs@gmail.com>
2026-03-23 15:32:17 -0400

    refactored dashboard panel by adding a helper fuction called renderBarChart that renders the bar charts that way there isnt 4 very similar looking use effect blocks

--- src/components/DashboardPanel.jsx
+++ src/components/DashboardPanel.jsx
@@ -1,106 +1,86 @@
-import { sum } from "d3";
 import Panel from "./Panel.jsx";
 import ClusterHeatmap from "./ClusterHeatmap.jsx";
 import { select } from 'https://esm.sh/d3-selection';
-import { useRef, useEffect, use } from "react";
+import { useRef, useEffect } from "react";
+
+function renderBarChart(barsRef, labelsRef, countsRef, data, color, isAverage = false ) {
+  if (!data || data.lenght === 0) return;
+
+  const maxCount = data[0].count;
+
+  select(barsRef.current)
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("height", 19)
+    .attr("width", (d) => (maxCount === 0 ? 0 : (d.count * 200) / maxCount))
+    .attr("y", (_, i) => i * 20)
+    .attr("fill", color);
+
+  select(labelsRef.current)
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .attr("y", (_, i) => i * 20 + 13)
+    .text((d) => d.primary_type)
+    .attr("text-anchor", "end");
+
+  select(countsRef.current)
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .attr("y", (_, i) => i * 20 + 13)
+    .text((d) => (isAverage ? d.count.toFixed(2) : d.count))
+    .attr("text-anchor", "start");
+}
+
+/** Literally just capitalizes the first letter of a string for better formatting */
+function capitalizeFirst(string) {
+  if (!string) return "";
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function DashboardPanel({ mode, selection, inactiveMode, inactiveSelection, activeSummary, inactiveSummary, pastDays, futureDays, heatData, targetHeatData, activeLoading, inactiveLoading }) {
   const barsRef = useRef();
+  const labelsRef = useRef();
+  const countsRef = useRef();
+
   const actualBarsRef = useRef();
-  const averageBarsRef = useRef();
-  const averageActualBarsRef = useRef();
+  const actualLabelsRef = useRef();
+  const actualCountsRef = useRef();
+
+  const avgBarsRef = useRef();
+  const avgLabelsRef = useRef();
+  const avgCountsRef = useRef();
+
+  const avgActualBarsRef = useRef();
+  const avgActualLabelsRef = useRef();
+  const avgActualCountsRef = useRef();
+
   const hasActive = Boolean(selection);
   const hasInactive = Boolean(inactiveSelection);
 
-  //Literally just capitalizes the first letter of a string for better formatting
-  const cFL = (string) => {
-    if (!string) return ''; // Handle empty or null strings
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-  //move inputs to constants to stop errors when null
   const summary = activeSummary?.top_types ?? null;
   const actual = inactiveSummary?.top_types ?? null;
-  const averageSummary = activeSummary?.top_types.map(type => ({ ...type, count: type.count / pastDays })) ?? null;
-  const averageActual = inactiveSummary?.top_types.map(type => ({ ...type, count: type.count / futureDays })) ?? null;
-
-  //get all the actual data from both past and "future" (Our past but future from anchor date perspective)
-  const combined = summary && actual ? (Object.values(summary.concat(actual).reduce((acc, {primary_type, count}) => {
-    acc[primary_type] = acc[primary_type] 
-    ? { ...acc[primary_type], count: acc[primary_type].count + count }
-    : { primary_type, count };
-  return acc;
-}, {})).sort((a, b) => b.count - a.count).slice(0, 10)) : null;
-  //console.log("Combined Summary", combined);;
+  const averageSummary = activeSummary?.top_types?.map((t) => ({ ...t, count: t.count / pastDays })) ?? null;
+  const averageActual = inactiveSummary?.top_types?.map((t) => ({ ...t, count: t.count / futureDays })) ?? null;
 
   //maps the source data to the source map graph in source map stats below
-  useEffect(() => { if (!summary) return;
-     const bars = select(barsRef.current);
-      bars.selectAll("rect").data(summary).join("rect").attr("height", 19).attr("width", d => d.count * 200/summary[0].count).attr("y", (d, i) => i * 20).attr("fill", "steelblue");
-      select('.labels').selectAll('text').data(summary).join('text').attr('y', function(d, i) {
-		    return i * 20 + 13;
-	    })
-	    .text(function(d) {
-		    return d.primary_type;
-	    }).attr("text-anchor", "end");
-      select('.counts').selectAll('text').data(summary).join('text').attr('y', function(d, i) {
-		    return i * 20 + 13;
-	    })
-	    .text(function(d) {
-		    return d.count;
-	    }).attr("text-anchor", "start");
+  useEffect(() => { 
+    renderBarChart(barsRef, labelsRef, countsRef, summary, "steelblue");
     }, [summary]);
 
   //maps the actual data to the actual map graph in Actual Data Map Stats below
-  useEffect(() => { if (!actual) return;
-     const bars = select(actualBarsRef.current);
-      bars.selectAll("rect").data(actual).join("rect").attr("height", 19).attr("width", d => d.count * 200/actual[0].count).attr("y", (d, i) => i * 20).attr("fill", "lightcoral");
-      select('.actualLabels').selectAll('text').data(actual).join('text').attr('y', function(d, i) {
-		    return i * 20 + 13;
-	    })
-	    .text(function(d) {
-		    return d.primary_type;
-	    }).attr("text-anchor", "end");
-      select('.actualCounts').selectAll('text').data(actual).join('text').attr('y', function(d, i) {
-		    return i * 20 + 13;
-	    })
-	    .text(function(d) {
-		    return d.count;
-	    }).attr("text-anchor", "start");
+  useEffect(() => {
+    renderBarChart(actualBarsRef, actualLabelsRef, actualCountsRef, actual, "lightcoral");
     }, [actual]);
 
-  useEffect(() => { if (!averageSummary) return;
-     const bars = select(averageBarsRef.current);
-      bars.selectAll("rect").data(averageSummary).join("rect").attr("height", 19).attr("width", d => {if (d.count === 0) return 0; return d.count * 200/averageSummary[0].count;}).attr("y", (d, i) => i * 20).attr("fill", "steelblue");
-      select('.averageLabels').selectAll('text').data(averageSummary).join('text').attr('y', function(d, i) {
-        return i * 20 + 13;
-      })
-      .text(function(d) {
-        return d.primary_type;
-      }).attr("text-anchor", "end");
-      select('.averageCounts').selectAll('text').data(averageSummary).join('text').attr('y', function(d, i) {
-        return i * 20 + 13;
-      })
-      .text(function(d) {
-        return d.count.toFixed(2);
-      }).attr("text-anchor", "start");
+  useEffect(() => { 
+    renderBarChart(avgBarsRef, avgLabelsRef, avgCountsRef, averageSummary, "steelblue", true);
     }, [averageSummary]);
 
-  useEffect(() => { if (!averageActual) return;
-      const bars = select(averageActualBarsRef.current);
-      bars.selectAll("rect").data(averageActual).join("rect").attr("height", 19).attr("width", d => {if (d.count === 0) return 0; return d.count * 200/averageActual[0].count;}).attr("y", (d, i) => i * 20).attr("fill", "lightcoral");
-      select('.averageActualLabels').selectAll('text').data(averageActual).join('text').attr('y', function(d, i) {
-        return i * 20 + 13;
-      })
-      .text(function(d) {
-        return d.primary_type;
-      }).attr("text-anchor", "end");
-      select('.averageActualCounts').selectAll('text').data(averageActual).join('text').attr('y', function(d, i) {
-        return i * 20 + 13;
-      })
-      .text(function(d) {
-        return d.count.toFixed(2);
-      }).attr("text-anchor", "start");
+  useEffect(() => {
+    renderBarChart(avgActualBarsRef, avgActualLabelsRef, avgActualCountsRef, averageActual, "lightcoral", true);
     }, [averageActual]);
 
   return (
@@ -122,7 +102,7 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                 <div>
                   {/* Source Map Stats */}
                   <p style={{ opacity: 0.95, margin: 0 }}>
-                    <strong>{cFL(selection.mode)}:</strong> Current stats for{" "}
+                    <strong>{capitalizeFirst(selection.mode)}:</strong> Current stats for{" "}
                     <strong>{selection.name}</strong>.
                     </p>
                     {activeLoading ? (
@@ -134,9 +114,9 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                         <strong>Top Crime Types by count:</strong>
                         <br/>
                         <svg width="430" height="240">
-                          <g className="bars" ref={barsRef} transform="translate(210, 30)"></g>
-                          <g className="labels" transform="translate(198, 30)" style={{fill: "white"}}></g>
-                          <g className="counts" transform="translate(220, 32)" style={{fill: "white"}}></g>
+                          <g ref={barsRef} transform="translate(210, 30)"/>
+                          <g ref={labelsRef} transform="translate(198, 30)" style={{fill: "white"}}/>
+                          <g ref={countsRef} transform="translate(220, 32)" style={{fill: "white"}}/>
                         </svg>
                       </div>
                         )}
@@ -145,9 +125,9 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                         <strong>Top Crime Types by average/day:</strong>
                         <br/>
                         <svg width="430" height="240">
-                          <g className="averageBars" ref={averageBarsRef} transform="translate(210, 30)"></g>
-                          <g className="averageLabels" transform="translate(198, 30)" style={{fill: "white"}}></g>
-                          <g className="averageCounts" transform="translate(220, 32)" style={{fill: "white"}}></g>
+                          <g ref={avgBarsRef} transform="translate(210, 30)"/>
+                          <g ref={avgLabelsRef} transform="translate(198, 30)" style={{fill: "white"}}/>
+                          <g ref={avgCountsRef} transform="translate(220, 32)" style={{fill: "white"}}/>
                         </svg>
                       </div>
                     )}
@@ -158,7 +138,7 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                 <div>
                   {/* Relation Map Stats */}
                   <p style={{ opacity: 0.9, margin: 0 }}>
-                    <strong>{cFL(selection.mode)}</strong> ready to compute stats for{" "}
+                    <strong>{capitalizeFirst(selection.mode)}</strong> ready to compute stats for{" "}
                     <strong>{selection.name}</strong>.
                   </p>
                 </div>
@@ -166,7 +146,7 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                 <div>
                   {/* Instance Map Stats */}
                   <p style={{ opacity: 0.9, margin: 0 }}>
-                    <strong>{cFL(selection.mode)}-level</strong> ready to compute stats for{" "}
+                    <strong>{capitalizeFirst(selection.mode)}-level</strong> ready to compute stats for{" "}
                     <strong>{selection.name}</strong>.
                   </p>
                 </div>
@@ -181,7 +161,7 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
               {hasInactive ? (inactiveSelection.mode === "actual" ? (
                 <div>
                   <p  style={{ opacity: 0.9, margin: 0 }}>
-                    <strong>{cFL(inactiveSelection.mode)}</strong> stats for{" "}
+                    <strong>{capitalizeFirst(inactiveSelection.mode)}</strong> stats for{" "}
                     <strong>{inactiveSelection.name}</strong>.
                   </p>
                   {inactiveLoading ? (
@@ -193,9 +173,9 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                         <strong>Top Crime Types by count:</strong>
                         <br/>
                         <svg width="430" height="240">
-                          <g className="actualBars" ref={actualBarsRef} transform="translate(210, 30)"></g>
-                          <g className="actualLabels" transform="translate(198, 30)" style={{fill: "white"}}></g>
-                          <g className="actualCounts" transform="translate(220, 32)" style={{fill: "white"}}></g>
+                          <g ref={actualBarsRef} transform="translate(210, 30)"/>
+                          <g ref={actualLabelsRef} transform="translate(198, 30)" style={{fill: "white"}}/>
+                          <g ref={actualCountsRef} transform="translate(220, 32)" style={{fill: "white"}}/>
                         </svg>
                       </div>
                       )}
@@ -204,9 +184,9 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
                         <strong>Top Crime Types by average/day:</strong>
                         <br/>
                         <svg width="430" height="240">
-                          <g className="averageActualBars" ref={averageActualBarsRef} transform="translate(210, 30)"></g>
-                          <g className="averageActualLabels" transform="translate(198, 30)" style={{fill: "white"}}></g>
-                          <g className="averageActualCounts" transform="translate(220, 32)" style={{fill: "white"}}></g>
+                          <g ref={avgActualBarsRef} transform="translate(210, 30)"/>
+                          <g ref={avgActualLabelsRef} transform="translate(198, 30)" style={{fill: "white"}}/>
+                          <g ref={avgActualCountsRef} transform="translate(220, 32)" style={{fill: "white"}}/>
                         </svg>
                       </div>
                     )}
@@ -216,7 +196,7 @@ export default function DashboardPanel({ mode, selection, inactiveMode, inactive
               ):(
                 <p  style={{ opacity: 0.9, margin: 0 }}>
                   {/* Target or Error Maps */}
-                  <strong>{cFL(inactiveSelection.mode)}</strong> ready to {inactiveSelection.mode === "target" ? "predict" : "compute"} stats for{" "}
+                  <strong>{capitalizeFirst(inactiveSelection.mode)}</strong> ready to {inactiveSelection.mode === "target" ? "predict" : "compute"} stats for{" "}
                   <strong>{inactiveSelection.name}</strong>.
                 </p>
               )) : (
